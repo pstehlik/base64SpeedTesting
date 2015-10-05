c4504cbb35c78cde9430f8f7364ffd7b18a23eb7
Philip Stehlik <philip.stehlik@taulia.com>
2015-10-05 08:41:54 -0700

    Making it easier to include other base64 methods for comparison

--- testBase64.groovy
+++ testBase64.groovy
@@ -16,19 +16,18 @@ println "shortTxt size [${shortTxt.length()}]"
 println "longTxt size [${longTxt.length()}]"
 
 
-void doWithGroovy(shortText, longText, iterations){
-
-	header("groovy", iterations)
+void encodeDecodeLoop(shortText, longText, iterations, libname, encode, decode){
+	header(libname, iterations)
 	def avg = 0
 	def single
 	def count = 0
 	iterations.times{ 
 		def start = System.currentTimeMillis()
-		String shortTextB64 = shortText.bytes.encodeBase64().toString()
-		String longTextB64 = longText.bytes.encodeBase64().toString()
+		String shortTextB64 = encode(shortText)
+		String longTextB64 = encode(longText)
 	
-		String sT = new String(shortTextB64.decodeBase64())
-		String lT = new String(longTextB64.decodeBase64())
+		String sT = decode(shortTextB64)
+		String lT = decode(longTextB64)
 
 		assert sT.length() == shortText.length()
 		assert lT.length() == longText.length()		
@@ -40,37 +39,8 @@ void doWithGroovy(shortText, longText, iterations){
 		count++
 	}
 	avg = avg / count
-	footer("groovy", avg)
+	footer(libname, avg)	
 }
-
-
-void doWithMig(shortText, longText, iterations){
-
-	header("migbase64", iterations)
-	def avg = 0
-	def single
-	def count = 0
-	iterations.times{ 
-		def start = System.currentTimeMillis()
-		String shortTextB64 = Base64.encodeToString(shortText.bytes, false)
-		String longTextB64 = Base64.encodeToString(longText.bytes, false)
-	
-		String sT = new String(Base64.decode(shortTextB64.bytes))
-		String lT = new String(Base64.decode(longTextB64.bytes))
-
-		assert sT.length() == shortText.length()
-		assert lT.length() == longText.length()		
-		
-		def end = System.currentTimeMillis()
-		def dur = end - start
-/*		println "[${count}] took [${dur}]ms"*/
-		avg += dur
-		count++
-	}
-	avg = avg / count
-	footer("migbase64", avg)
-}
-
 
 void header(which, iter){
 	println "$which: iterations [${iter}]"
@@ -80,14 +50,29 @@ void footer(which, avg){
 	println "$which: average [${avg}]ms"
 }
 
+
+def encodeGroovy = { String text ->
+	text.bytes.encodeBase64().toString()
+}
+
+def decodeGroovy = { String text ->
+	new String(text.decodeBase64())
+}
+
+def encodeMig = { String text ->
+	Base64.encodeToString(text.bytes, false)
+}
+
+def decodeMig = { String text ->
+	new String(Base64.decode(text.bytes))
+}
+
+int iters = 50
 println "--- warmup ---"
-doWithGroovy(shortTxt, longTxt, 50)
-doWithMig(shortTxt, longTxt, 50)
+encodeDecodeLoop(shortTxt, longTxt, iters, 'groovy', encodeGroovy, decodeGroovy)
+encodeDecodeLoop(shortTxt, longTxt, iters, 'migBase64', encodeMig, decodeMig)
 
+iters = 300
 println "--- for real ---"
-doWithGroovy(shortTxt, longTxt, 500)
-doWithMig(shortTxt, longTxt, 500)
-
-println "--- for real again ---"
-doWithMig(shortTxt, longTxt, 500)
-doWithGroovy(shortTxt, longTxt, 500)
+encodeDecodeLoop(shortTxt, longTxt, iters, 'groovy', encodeGroovy, decodeGroovy)
+encodeDecodeLoop(shortTxt, longTxt, iters, 'migBase64', encodeMig, decodeMig)
